db0ece8c6c38782886464d10a45678558914499a
Grzegorz Wierzowiecki <gwgithub23@wierzowiecki.pl>
2023-11-11 19:33:30 +0100

    Added TTS example script.

---
+++ openai_docs_samples/TTS_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from openai import OpenAI
+
+client = OpenAI()
+
+response = client.audio.speech.create(
+    model="tts-1",
+    voice="alloy",
+    input="Hello world! This is a streaming test.",
+)
+
+response.stream_to_file("output.mp3")
